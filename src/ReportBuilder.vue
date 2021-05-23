02ee9c7f738d9ef9530988ee38cb989e4366a92c
Mungunbagana Batzorig <MB.Bagana@gmail.com>
2021-05-23 20:14:33 +0800

    builder

--- src/ReportBuilder.vue
+++ src/ReportBuilder.vue
@@ -1,34 +1,18 @@
 <template>
     <section class="report-builder">
         <Spin size="large" fix v-if="loading"></Spin>
-        <div class="rb-sidebar">
-            <div class="rb-control">
-                <div class="rb-control-item">
-                    <label>Тайлан нэр</label>
-                    <Input v-model="reportName" placeholder="Тайлан нэр"/>
-                </div>
-            </div>
-
-            <div class="rb-submit">
-                <Button type="success" long @click="saveReport">{{`${$static_words ? $static_words.save : 'Хадгалах'}`}}</Button>
-            </div>
-        </div>
-
         <div class="rb-workspace">
-            <Tabs type="card" :animated="false">
-                <TabPane label="Үндсэн тохиргоо" :key="`main-tab`" icon="md-code-working">
                     <div id="designer-host">
                         <JSDesigner v-bind:report="{id: 'report.rdlx-json', displayName: 'lambda report' }"></JSDesigner>
                     </div>
-                </TabPane>
-                <!-- Formula confiration -->
-            </Tabs>
         </div>
     </section>
 </template>
 
 <script>
 import { Designer } from "@grapecity/activereports-vue";
+import '@grapecity/activereports/styles/ar-js-ui.css';
+import '@grapecity/activereports/styles/ar-js-designer.css';
 export default {
     name:"Lambda report designer",
     components: {
@@ -46,6 +30,7 @@ export default {
     }
 };
 </script>
+
 <style>
 #designer-host {
     width: 100%;
