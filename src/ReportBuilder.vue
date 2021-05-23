3c799931f3a41aeb871d3a5c84bb4642ce2cc991
Mungunbagana Batzorig <MB.Bagana@gmail.com>
2021-05-23 22:22:53 +0800

    no license btn is hidden

--- src/ReportBuilder.vue
+++ src/ReportBuilder.vue
@@ -11,6 +11,8 @@
 
 <script>
 import { Designer } from "@grapecity/activereports-vue";
+import { Core } from "@grapecity/activereports";
+Core.setLicenseKey("l6/zrbWoSbcLFwEetFh38rH3ErBZE9H+Cqix3R+wTlfA1wD5B+lUcCQn+EJ60I4RGrm0x1sFjkiLWwB0jAn6BWZv0W4WbqAKriOdeoivxDp1Wmjs3qkEDhvbsjPtfvwx2BHil6o+/tDrdMJQSGs18WZm2PoQLQuL+9VhZ4FNRHUQU3Jtioke/OZEGHJOdYVwvCGalzBad6QFOiVbDBQPePpS3++GJzOxN8SN/7lyS5/IdKiy3WJRaVGkB370+HbN6hKraDfUgReLX26yxRaKC/5aWnGAJ2NnWLoGyAGRcwT9dVjo4bcAZNrrA0U9JVKQxaSskhdv2p49XzJkltXx5w==");
 import '@grapecity/activereports/styles/ar-js-ui.css';
 import '@grapecity/activereports/styles/ar-js-designer.css';
 export default {
