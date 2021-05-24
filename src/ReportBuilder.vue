1e2c8f86719ae55be09fc62abf08d0ec2ddf424c
Mungunbagana Batzorig <MB.Bagana@gmail.com>
2021-05-24 14:58:57 +0800

    testing

--- src/ReportBuilder.vue
+++ src/ReportBuilder.vue
@@ -2,33 +2,128 @@
     <section class="report-builder">
         <Spin size="large" fix v-if="loading"></Spin>
         <div class="rb-workspace">
-                    <div id="designer-host">
-                        <JSDesigner v-bind:report="{id: 'report.rdlx-json', displayName: 'lambda report' }"></JSDesigner>
-                    </div>
+            <div id="designer-host"
+                 :style="{ display: designerHidden ? 'none' : 'block' }">
+                <JSDesigner v-bind:report="{id: 'report.rdlx-json', displayName: 'lambda report' }"
+                            v-bind:onCreate="onCreateReport"
+                            v-bind:onSave="onSaveReport"
+                            v-bind:onSaveAs="onSaveAsReport"
+                            v-bind:onOpen="onOpenReport"></JSDesigner>
+            </div>
+            <div
+                id="viewer-host"
+                :style="{ display: designerHidden ? 'block' : 'none' }"
+            >
+                <ReportViewer ref="reportViewer"/>
+            </div>
         </div>
     </section>
 </template>
 
 <script>
-import { Designer } from "@grapecity/activereports-vue";
-import { Core } from "@grapecity/activereports";
+import {templates} from "@grapecity/activereports/reportdesigner";
+import {Viewer, Designer} from "@grapecity/activereports-vue";
+import {Core} from "@grapecity/activereports";
+
 Core.setLicenseKey("l6/zrbWoSbcLFwEetFh38rH3ErBZE9H+Cqix3R+wTlfA1wD5B+lUcCQn+EJ60I4RGrm0x1sFjkiLWwB0jAn6BWZv0W4WbqAKriOdeoivxDp1Wmjs3qkEDhvbsjPtfvwx2BHil6o+/tDrdMJQSGs18WZm2PoQLQuL+9VhZ4FNRHUQU3Jtioke/OZEGHJOdYVwvCGalzBad6QFOiVbDBQPePpS3++GJzOxN8SN/7lyS5/IdKiy3WJRaVGkB370+HbN6hKraDfUgReLX26yxRaKC/5aWnGAJ2NnWLoGyAGRcwT9dVjo4bcAZNrrA0U9JVKQxaSskhdv2p49XzJkltXx5w==");
+import {PageReport} from "@grapecity/activereports/core";
+import {exportDocument as PdfExport} from "@grapecity/activereports/pdfexport";
+import {FontStore} from "@grapecity/activereports/core";
 import '@grapecity/activereports/styles/ar-js-ui.css';
 import '@grapecity/activereports/styles/ar-js-designer.css';
+import '@grapecity/activereports/styles/ar-js-viewer.css';
+
 export default {
-    name:"Lambda report designer",
+    name: "Lambda report designer",
     components: {
-        JSDesigner: Designer,
+        JSDesigner: Designer, ReportViewer: Viewer,
     },
-    data(){
-        return{
-            loading:false,
-            reportName:null,
-            reportJson:null,
+    computed: {
+        reportIds() {
+            const ret = this.counter ? [...this.reportStorage.keys()] : [];
+            return ret;
+        },
+    },
+    data() {
+        return {
+            loading: false,
+            reportName: null,
+            reportJson: null,
+            reportStorage: new Map(),
+            counter: 0,
+            designerHidden: false,
         }
     },
-    methods:{
-        saveReport(){}
+    mounted() {
+        var designButton = {
+            key: "$openDesigner",
+            text: "Edit in Designer",
+            iconCssClass: "mdi mdi-pencil",
+            enabled: true,
+            action: () => {
+                this.designerHidden = false;
+            },
+        };
+        var viewer = this.$refs.reportViewer.Viewer();
+        viewer.toolbar.addItem(designButton);
+        viewer.toolbar.updateLayout({
+            default: [
+                "$openDesigner",
+                "$split",
+                "$navigation",
+                "$split",
+                "$refresh",
+                "$split",
+                "$history",
+                "$split",
+                "$zoom",
+                "$fullscreen",
+                "$split",
+                "$print",
+                "$split",
+                "$singlepagemode",
+                "$continuousmode",
+                "$galleymode",
+            ],
+        });
+    },
+    methods: {
+        onCreateReport() {
+            const reportId = `NewReport${this.counter + 1}`;
+            this.counter++;
+            return Promise.resolve({
+                definition: templates.CPL,
+                id: reportId,
+                displayName: reportId,
+            });
+        },
+        onSaveReport(info) {
+            const reportId = info.id || `NewReport${this.counter + 1}`;
+            this.reportStorage.set(reportId, info.definition);
+            this.counter++;
+            return Promise.resolve({displayName: reportId});
+        },
+        onSaveAsReport(info) {
+            const reportId = `NewReport${this.counter + 1}`;
+            this.reportStorage.set(reportId, info.definition);
+            this.counter++;
+            return Promise.resolve({id: reportId, displayName: reportId});
+        },
+        async onPdfPreview() {
+            const reportInfo = await this.$refs.reportDesigner.getReport();
+            const report = new PageReport();
+            await report.load(reportInfo.definition);
+            const doc = await report.run();
+            const result = await PdfExport(doc);
+            result.download("exportedreport");
+        },
+        onDesignerOpen() {
+            this.designerHidden = false;
+        },
+        async onReportPreview(report) {
+            this.designerHidden = true;
+            this.$refs.reportViewer.Viewer().open(report.definition);
+        },
     }
 };
 </script>
